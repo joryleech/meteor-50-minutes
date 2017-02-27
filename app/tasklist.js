e4d2e7d8cdfc10dc756131d73afae66308508c10
Jory Leech <joryleech@gmail.com>
2017-02-26 21:58:45 -1000

    Task 2: Tasklist Setup

---
+++ app/tasklist.js
@@ -0,0 +1,18 @@
+/**
+ * Created by epita on 2/26/2017.
+ */
+
+
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+}
